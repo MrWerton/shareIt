682c5a2b09bd95bcb26f497bdf1ed7daf2a06ea2
werton <mr.werton@gmail.com>
2023-09-28 03:03:30 -0300

    feat: created typer text animation

---
+++ src/shared/components/TyperAnimation/styles.ts
@@ -0,0 +1,15 @@
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
+
+interface AnimationTextProps{
+    isTitle?: boolean,
+    fontSize?: string,
+    color?: string,
+}
+
+export const TextAnimation = styled(motion.p)<AnimationTextProps>`
+  color: #eee;
+  font-size: ${props=>props.fontSize ?? '1.5rem'};
+  color: ${props=>props.color ?? '#222'};
+  font-weight: ${props=>props.isTitle?"bold":"normal"};
+`;
